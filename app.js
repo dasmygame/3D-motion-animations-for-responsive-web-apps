0095eb235ff43e69d534eef9508cdff2d836e012
Rahul Das <48290252+dasmygame@users.noreply.github.com>
2022-11-03 15:25:56 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-<div id="background-wrap">
-    <div class="bubble x1"></div>
-    <div class="bubble x2"></div>
-    <div class="bubble x3"></div>
-    <div class="bubble x4"></div>
-    <div class="bubble x5"></div>
-    <div class="bubble x6"></div>
-    <div class="bubble x7"></div>
-    <div class="bubble x8"></div>
-    <div class="bubble x9"></div>
-    <div class="bubble x10"></div>
-</div>
+import React, { Component } from 'react';
+import './animation.css'; // Tell webpack that Button.js uses these styles
+
+class Button extends Component {
+  render() { 
+        <div id="background-wrap">
+        <div class="bubble x1"></div>
+        <div class="bubble x2"></div>
+        <div class="bubble x3"></div>
+        <div class="bubble x4"></div>
+        <div class="bubble x5"></div>
+        <div class="bubble x6"></div>
+        <div class="bubble x7"></div>
+        <div class="bubble x8"></div>
+        <div class="bubble x9"></div>
+        <div class="bubble x10"></div>
+    </div>
+  }
+}
